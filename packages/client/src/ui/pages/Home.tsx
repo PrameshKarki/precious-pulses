87e01256cd943fba6c9acda2a441784131eea858
Pramesh Karki <prameshkarki0407@gmail.com>
2023-08-23 09:04:28 +0545

    add: basic design and remote procedures

--- packages/client/src/ui/pages/Home.tsx
+++ packages/client/src/ui/pages/Home.tsx
@@ -1,10 +1,42 @@
-import { trpc } from "../../lib/trpc"
+import moment from "moment";
+import { trpc } from "../../lib/trpc";
 
 const Home = () => {
-  const response=trpc.pulse.getPulses.useQuery()
+  const response=trpc.pulse.getPulses.useQuery( )
+  const randomPulse=trpc.pulse.createPulse.useMutation({
+    onSuccess:()=>response.refetch()
+  })
+  const deletePulse=trpc.pulse.deletePulse.useMutation({
+    onSuccess:()=>response.refetch()
+  })
   return (
-    <div>Home</div>
-  )
+    <div className="bg-white w-2/3 mx-auto ">
+      <h1 className="text-center font-medium text-xl">Precious Pulses</h1>
+      <section className="pulse-container py-3 relative">
+        {response.data?.map(el=>
+        <div className="flex justify-between bg-gray-50 px-4 py-4 my-2">
+          <div className="">
+            <h3 className="text-sm">{el?.title}</h3>
+            <p className="text-xs text-gray-700">{el?.description}</p>
+          </div>
+          <div>
+            <p className="text-sm">{el?.date ? moment(el?.date).format("YYYY-MM-DD") : ""}</p>
+            {el?.date &&  <p className="text-xs mr-10">{moment(el.date).fromNow()}</p>}
+          </div>
+          <button className="absolute p-1 text-white bg-red-700 right-0 bg-red text-xs" onClick={()=>{
+            deletePulse.mutate({
+              id:el.id
+            })
+          }}>Delete</button>
+        </div>
+
+        )}
+      </section>
+      <div className="flex justify-center items-center">
+        <button className="bg-green-600 text-white px-3 py-2 rounded-lg" onClick={()=>randomPulse.mutate()}>Generate Random</button>
+      </div>
+    </div>
+  );
 }
 
 export default Home 
--- packages/server/src/router/pulseRouter.ts
+++ packages/server/src/router/pulseRouter.ts
@@ -1,17 +1,29 @@
+import { faker } from '@faker-js/faker';
 import z from "zod";
 import prisma from "../lib/prisma";
 import { trpc } from "../lib/trpc";
 
+
 export const pulseRouter=trpc.router({
-    getPulses:trpc.procedure.query(async()=>{
-        const pulses=await prisma.pulse.findMany()
-        console.log("🚀 ~ file: pulseRouter.ts:7 ~ getPulses:trpc.procedure.query ~ pulses:", pulses)
-        return "Hello World!"
+    getPulses:trpc.procedure.query(()=>{
+        return prisma.pulse.findMany()
     }),
-    createPulse:trpc.procedure.input(z.object({
-        title:z.string(),
+    createPulse:trpc.procedure.mutation(async()=>{
+        return prisma.pulse.create({
+            data:{
+                date:faker.date.recent(),
+                description:faker.lorem.sentence(),
+                title:faker.lorem.sentence(),
+            }
+        })
+    }),
+    deletePulse:trpc.procedure.input(z.object({
+        id:z.number()
     })).mutation(async(data)=>{
-       console.log(data);
-       return true; 
-    })
+       return prisma.pulse.delete({
+            where:{
+                id:data.input.id
+            }
+        })
+    }),
 })
